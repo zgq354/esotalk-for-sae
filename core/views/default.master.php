24a0bb72f63830dc7b53cec2136400edd636a195
阿卡林酱 <kawaiiushio@gmail.com>
2015-07-12 16:58:34 +0800

    add UTC & World mainstream airport current time
    add UTC & World mainstream airport current time

--- core/views/default.master.php
+++ core/views/default.master.php
@@ -16,8 +16,7 @@ if (!defined("IN_ESOTALK")) exit;
 <meta charset='<?php echo T("charset", "utf-8"); ?>'>
 <title><?php echo sanitizeHTML($data["pageTitle"]); ?></title>
 <?php echo $data["head"]; ?>
-<link rel="icon" type="image/png" href="<?php echo getResource("core/skin/favicon.png");?>">
-<link rel="apple-touch-icon" href="<?php echo getResource("core/skin/apple-touch-icon.png");?>">
+v<link rel="apple-touch-icon" href="<?php echo getResource("core/skin/apple-touch-icon.png");?>">
 </head>
 
 <body class='<?php echo $data["bodyClass"]; ?>'>
@@ -72,6 +71,82 @@ if (!defined("IN_ESOTALK")) exit;
 <li id='goToTop'><a href='#'><?php echo T("Go to top"); ?></a></li>
 <?php echo $data["metaMenuItems"]; ?>
     <?php if (!empty($data["statisticsMenuItems"])) echo $data["statisticsMenuItems"]; ?> <?php $etime=microtime(true);$total=floor(($etime-PAGE_START_TIME)*1000)/1000;echo "{$total}s,"; ?> <?php $query = ET::$database->queries;$query = count($query);echo $query;?> sql queries
+      <script language="javascript" type="text/javascript">
+//https://gist.github.com/1326893https://gist.github.com/1326893
+//20120511 Geovin Du 塗聚文
+function calcTime(city, offset) {
+var d = new Date();
+utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+var nd = new Date(utc + (3600000 * offset));
+var gmtTime = new Date(utc)
+var day = nd.getDate();
+var month = nd.getMonth();
+var year = nd.getYear();
+var hr = nd.getHours(); //+ offset
+var min = nd.getMinutes();
+var sec = nd.getSeconds();
+if(year < 1000){
+year += 1900
+}
+var monthArray = new Array("January", "February", "March", "April", "May", "June", "July", "August",
+"September", "October", "November", "December")
+var monthDays = new Array("31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31")
+if (year%4 == 0){
+monthDays = new Array("31", "29", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31")
+}
+if(year%100 == 0 && year%400 != 0){
+monthDays = new Array("31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31")
+}
+if (hr >= 24){
+hr = hr-24
+day -= -1
+}
+if (hr < 0){
+hr -= -24
+day -= 1
+}
+if (hr < 10){
+hr = " " + hr
+}
+if (min < 10){
+min = "0" + min
+}
+if (sec < 10){
+sec = "0" + sec
+}
+if (day <= 0){
+if (month == 0){
+month = 11
+year -= 1
+}
+else{
+month = month -1
+}
+day = monthDays[month]
+}
+if(day > monthDays[month]){
+day = 1
+if(month == 11){
+month = 0
+year -= -1
+}
+else{
+month -= -1
+}
+}
+return city + hr + ":" + min
+//return "The local time in " + city + " is " + nd.toLocaleString()+;
+}
+function worldClockZone(){
+document.getElementById('localutc').innerHTML = calcTime('UTC', '-0');
+document.getElementById('LAX').innerHTML = calcTime('LAX', '-7');
+document.getElementById('PVG').innerHTML = calcTime('PVG ', '+8');
+document.getElementById('JFK').innerHTML = calcTime('JFK', '-4');
+setTimeout("worldClockZone()", 1000)
+}
+window.onload=worldClockZone;
+</script>
+<span id="localutc"></span> · <span id="LAX"></span> ·  <span id="PVG"></span> · <span id="JFK"></span>
 </ul>
 </div>
 </div>
